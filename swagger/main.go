5fd79f92b81a86279fc792c1c5d6083adf24a965
Tanapon Piasuwan <paisuwan16@gmail.com>
2024-03-04 21:06:25 +0700

    edit swager add: feature  post

--- swagger/main.go
+++ swagger/main.go
@@ -25,6 +25,8 @@ type HTTPError struct {
 	Description       string `json:"description"`
 }
 
+var pets []Pet // ประกาศตัวแปร global เพื่อเก็บข้อมูล Pet
+
 // @title Echo Swagger Example API
 // @version 1.0
 // @description This is a sample server server.
@@ -45,9 +47,10 @@ func main() {
 	e := echo.New()
 
 	e.GET("/swagger/*", echoSwagger.WrapHandler)
-
+	pets = append(pets, Pet{ID: 1, Name: "ChaNom"}, Pet{ID: 2, Name: "Mali"})
 	e.GET("/pets", allPet)
-	e.Logger.Fatal(e.Start(":1323"))
+	e.POST("/pets", createPet)
+	e.Logger.Fatal(e.Start(":8080"))
 }
 
 // ListPet godoc
@@ -62,8 +65,40 @@ func main() {
 // @Failure      500  {object}  HTTPError
 // @Router       /pets [get]
 func allPet(c echo.Context) error {
-	return c.JSON(http.StatusOK, []Pet{
-		{ID: 1, Name: "ChaNom"},
-		{ID: 2, Name: "Mali"},
-	})
+
+	return c.JSON(http.StatusOK, pets)
+}
+
+// CreatePet godoc
+// @Summary      Create a new pet
+// @Description  Create a new pet with the given information
+// @Tags         pet
+// @Accept       json
+// @Produce      json
+// @Param        pet body Pet true "Pet object that needs to be added to the store"
+// @Success      201  {object} Pet
+// @Failure      400  {object} HTTPError
+// @Failure      500  {object} HTTPError
+// @Router       /pets [post]
+func createPet(c echo.Context) error {
+	newPet, err := bindPetFromBody(c)
+	if err != nil {
+		return err
+	}
+	pets = append(pets, *newPet)
+
+	// ทำการบันทึก newPet ลงในฐานข้อมูลหรือทำการประมวลผลต่อไป
+
+	// ส่ง response กลับไป
+	return c.JSON(http.StatusCreated, newPet)
+}
+
+// bindPetFromBody ฟังก์ชันที่ใช้เพื่อแปลงข้อมูล JSON จาก request body เป็นโครงสร้างข้อมูล Pet
+func bindPetFromBody(c echo.Context) (*Pet, error) {
+	newPet := new(Pet)
+	if err := c.Bind(newPet); err != nil {
+		return nil, err
+	}
+
+	return newPet, nil
 }
